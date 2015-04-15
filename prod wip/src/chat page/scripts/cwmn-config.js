0ef16f8011891a3bb978b39966986edae8abb575
unknown <Tim.Smith@notamac.i3domain.inin.com>
2015-04-15 14:37:27 -0600

    implementing CWMN REST API instead of JS API

---
+++ prod wip/src/chat page/scripts/cwmn-config.js
@@ -0,0 +1,29 @@
+// Define the link options here
+var cwmnHostOptions = [
+		{
+			"text": "Landing Page",
+			"description": "A page to see stuff.",
+			"url": "https://tim-cic4su5.dev2000.com/cwmn2/landing.html"
+		},
+		{
+			"text": "CWMN",
+			"description": "Click With Me Now's main page.",
+			"url": "http://clickwithmenow.com"
+		}
+	];
+var cwmnGuestOptions = [
+		{
+			"text": "Landing Page",
+			"description": "A page to see stuff.",
+			"url": "https://tim-cic4su5.dev2000.com/cwmn2/landing.html"
+		},
+		{
+			"text": "CWMN",
+			"description": "Click With Me Now's main page where you can learn more about Click With Me Now.",
+			"url": "http://clickwithmenow.com"
+		}
+	];
+
+var screenDomain = 'tim-cic4su5.dev2000.com';
+var authorizationHeader = 'xPsCX7XXAPOdIeCON770z7v2bHykxZ5eAJnUKtzXjVQ0hDtJNgvd833xU3rvfv2M';
+var apiUrl = 'http://m.clickwith.me/session/inviteToHost';
